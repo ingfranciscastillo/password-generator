3ab99734a6f9c1783a5d37a4628a4ed746e70f85
Francis Miguel Castillo Cruz <franciscastillocruz@gmail.com>
2025-06-23 16:42:34 -0400

    feat: add Sonner Toaster component and update dependencies
    - Created a new Sonner Toaster component that integrates with next-themes for theme management.
    - Updated package.json to include new dependencies: next-themes and sonner.
    - Added @tanstack/react-query and @tanstack/eslint-plugin-query to dependencies and devDependencies respectively.

---
+++ app/theme-providers.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Toaster } from "@/components/ui/sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+
+const queryClient = new QueryClient();
+
+const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Toaster position="top-center" />
+      {children}
+    </QueryClientProvider>
+  );
+};
+
+export default ThemeProvider;
